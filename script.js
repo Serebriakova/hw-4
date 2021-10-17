2cdaf89c8288b51fe35e9b16f117514fdbaf9727
Serebriakova <serebriakova.alla@gmail.com>
2021-10-17 12:28:15 +0300

    update

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const operation = prompt("Enter math operation: add, sub, mult, div");
-let number1 = prompt("Enter number 1");
-let number2 = prompt("Enter number 2");
+const number1 = prompt("Enter number 1");
+const number2 = prompt("Enter number 2");
 
-let add = Number(number1) + Number(number2);
-let sub = number1 - number2;
-let mult = number1 * number2;
-let div = number1 / number2;
+const add = Number(number1) + Number(number2);
+const sub = number1 - number2;
+const mult = number1 * number2;
+const div = number1 / number2;
 
 if (operation == "add") {
     alert(`${number1} + ${number2} = ${add} `);
